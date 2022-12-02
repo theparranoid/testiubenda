14d0095075fa37ef4dddaa23c2eaf3d28fa04126
parranoid <andfer220@gmail.com>
2022-12-02 10:04:17 +0600

    add couple basic tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,11 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended"
   ],
+  env: {
+    "vitest-globals/env": true
+  },
   parserOptions: {
     ecmaVersion: "latest",
   },
---
+++ src/helpers/flattenObject.test.js
@@ -0,0 +1,14 @@
+import flattenObject from "@/helpers/flattenObject";
+describe('flattenObject', () => {
+  it('should flatten the object', () => {
+    const testObj = {
+      abc: 1,
+      cba: {
+        cba: 2
+      }
+    };
+    const expectedOutput = {abc:1, cba: 2}
+    const result = flattenObject(testObj);
+    expect(result).toEqual(expectedOutput);
+  })
+})
